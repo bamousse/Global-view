767fce6267a27bf8bc6f49859a123aa8d81383c2
bamousse <53902749+bamousse@users.noreply.github.com>
2020-08-27 15:04:15 +0100

    Update source-project.tf

--- source-project.tf
+++ source-project.tf
@@ -1,11 +1,11 @@
 module "lambda-Call" {
   source = "Modules/lambda"
 
-  function_name      = "lambda-instance-state"
-  filename           = "start_instance.zip"
-  description        = "Actions On instance state"
-  handler            = "start_instance.lambda_handler"
-  memory_size        = "128"
-  concurrency        = "5"
-  role_arn           = "some_arn_role"
+  function_name      = var.function_name
+  filename           = var.filename
+  description        = var.description
+  handler            = var.handler
+  memory_size        = var.memory_size
+  concurrency        = var.concurrency
+  role_arn           = var.arn
 }
