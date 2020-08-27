8f0a7cb2346a61ba2b3c87685e3f52ff6dd987a7
bamousse <53902749+bamousse@users.noreply.github.com>
2020-08-27 15:16:23 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,2 +1,37 @@
-variable "access_key" {}
-variable "secret_key" {}
+variable "function_name" {
+  type = string
+}
+
+variable "filename" {
+  type = string
+}
+
+variable "description" {
+  type = string
+}
+
+ variable "description" {
+  type = string
+}
+
+variable "handler" {
+  description = "format should be of : filename.lambda_handler (filename without .zip extension) "
+}
+
+ variable "memory_size" {
+  type = number
+}
+
+ variable "concurrency" {
+  type = number
+}
+
+variable "role_arn " {}
+
+
+
+  
+  
+  
+  
+  
