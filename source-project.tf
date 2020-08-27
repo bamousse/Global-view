596c81ef643a4dedf1277346256b798208c244ef
bamousse <53902749+bamousse@users.noreply.github.com>
2020-08-27 14:22:49 +0100

    Update source-project.tf

--- source-project.tf
+++ source-project.tf
@@ -2,10 +2,10 @@ module "lambda-Call" {
   source = "Modules/lambda"
 
   function_name      = "lambda-instance-state"
-  filename           = "../start_instance.zip"
+  filename           = "start_instance.zip"
   description        = "Actions On instance state"
   handler            = "start_instance.lambda_handler"
   memory_size        = "128"
   concurrency        = "5"
-  role_arn           = "arn:aws:iam::993793825572:role/lambda-admin-accessTest"
+  role_arn           = "some_arn_role"
 }
